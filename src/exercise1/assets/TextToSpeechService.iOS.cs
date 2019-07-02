b04b94093ce9689c93a39bf57e4055764c7b1a1b
Chris v Wyk <chris@jib.co.za>
2019-07-02 11:27:22 +0200

    Fix for CUSTOMER FEEDBACK 96616 (adding assets)

---
+++ src/exercise1/assets/TextToSpeechService.iOS.cs
@@ -0,0 +1,19 @@
+﻿using AVFoundation;
+
+namespace GreatQuotes.iOS
+{
+	public class TextToSpeechService : ITextToSpeech
+	{
+		public void Speak(string text)
+		{
+			var speechSynthesizer = new AVSpeechSynthesizer();
+			speechSynthesizer.SpeakUtterance(new AVSpeechUtterance(text) {
+                Rate = AVSpeechUtterance.DefaultSpeechRate,
+				Voice = AVSpeechSynthesisVoice.FromLanguage ("en-US"),
+				Volume = .5f,
+				PitchMultiplier = 1.0f
+			});
+		}
+	}
+}
+
